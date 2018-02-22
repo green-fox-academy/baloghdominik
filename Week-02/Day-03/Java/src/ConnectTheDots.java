74b09ff761a75a8389a9361500e2c6ceebddfe23
baloghdominik <balogh0dominik@gmail.com>
2018-02-22 11:46:31 +0100

    feat():

--- Week-02/Day-03/Java/src/ConnectTheDots.java
+++ Week-02/Day-03/Java/src/ConnectTheDots.java
@@ -12,9 +12,24 @@ public class ConnectTheDots {
         // Connect these to get a box: {{10, 10}, {290,  10}, {290, 290}, {10, 290}}
         // Connect these: {{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70},
         // {120, 100}, {85, 130}, {50, 100}}
+        int[][] array1 = new int[][] {{10, 10}, {290,  10}, {290, 290}, {10, 290}};
+        int[][] array2 = new int[][] {{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70}, {120, 100}, {85, 130}, {50, 100}};
+
+        dotsconnection(graphics,array1);
+        dotsconnection(graphics,array2);
 
 
+    }
 
+    private static void dotsconnection(Graphics graphics, int[][] array1) {
+        graphics.setColor(Color.green);
+        for (int i = 0; i < array1.length; i++) {
+            if (i == array1.length - 1) {
+                graphics.drawLine(array1[i][0], array1[i][1], array1[0][0], array1[0][1]);
+            } else {
+                graphics.drawLine(array1[i][0], array1[i][1], array1[i + 1][0], array1[i + 1][1]);
+            }
+        }
     }
 
     // Don't touch the code below
