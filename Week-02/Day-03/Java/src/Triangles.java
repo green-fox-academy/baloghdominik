bc8ee1d782d0807ab5db86429444a2137085ae2a
baloghdominik <balogh0dominik@gmail.com>
2018-02-23 14:07:36 +0100

    fix(Triangles)create functions, color changing

--- Week-02/Day-03/Java/src/Triangles.java
+++ Week-02/Day-03/Java/src/Triangles.java
@@ -1,57 +1,27 @@
 import javax.swing.*;
 import java.awt.*;
-import java.util.Random;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class Triangles {
     public static void mainDraw(Graphics graphics) {
-
-
         triangles(graphics);
-
-
     }
 
     private static void triangles(Graphics graphics) {
-        Random rand = new Random();
-        int R = (int)(Math.random( )*256);
-        int G = (int)(Math.random( )*256);
-        int B = (int)(Math.random( )*256);
-        Color randomColor = new Color(R, G, B);
-        graphics.setColor(randomColor);
+        Colors(graphics);
+        MainTriangle(graphics);
+        HorizontalLines(graphics);
+        LeftToRightLines(graphics);
+        RightToLeftLines(graphics);
+    }
 
-        graphics.drawLine(200, 20,100,190);
-        graphics.drawLine(100, 190,300,190);
-        graphics.drawLine(300, 190,200,20);
+    private static void RightToLeftLines(Graphics graphics) {
+        int x1 = 210;
+        int y1 = 37;
+        int x2 = 120;
+        int y2 = 190;
 
-        int x1 = 200;
-        int y1 = 20;
-        int x2 = 200;
-        int y2 = 20;
-        for (int i = 0; i < 10; i++) {
-            x1 -= 10;
-            y1 += 17;
-            x2 += 10;
-            y2 += 17;
-            graphics.drawLine(x1, y1,x2,y2);
-        }
-
-        x1 = 190;
-        y1 = 37;
-        x2 = 280;
-        y2 = 190;
-        for (int i = 0; i < 9; i++) {
-            graphics.drawLine(x1,y1,x2,y2);
-            x1 -= 10;
-            y1 += 17;
-            x2 -= 20;
-        }
-
-        x1 = 210;
-        y1 = 37;
-        x2 = 120;
-        y2 = 190;
         for (int i = 0; i < 9; i++) {
             graphics.drawLine(x1,y1,x2,y2);
             x1 += 10;
@@ -60,10 +30,52 @@ public class Triangles {
         }
     }
 
-    // Don't touch the code below
+    private static void LeftToRightLines(Graphics graphics) {
+        int x1 = 190;
+        int y1 = 37;
+        int x2 = 280;
+        int y2 = 190;
+
+        for (int i = 0; i < 9; i++) {
+            graphics.drawLine(x1,y1,x2,y2);
+            x1 -= 10;
+            y1 += 17;
+            x2 -= 20;
+        }
+    }
+
+    private static void HorizontalLines(Graphics graphics) {
+        int x1 = 200;
+        int y1 = 20;
+        int x2 = 200;
+        int y2 = 20;
+
+        for (int i = 0; i < 10; i++) {
+            x1 -= 10;
+            y1 += 17;
+            x2 += 10;
+            y2 += 17;
+            graphics.drawLine(x1, y1,x2,y2);
+        }
+    }
+
+    private static void MainTriangle(Graphics graphics) {
+        graphics.drawLine(200, 20,100,190);
+        graphics.drawLine(100, 190,300,190);
+        graphics.drawLine(300, 190,200,20);
+    }
+
+    private static void Colors(Graphics graphics) {
+        int R = (int)(Math.random( )*256);
+        int G = (int)(Math.random( )*256);
+        int B = (int)(Math.random( )*256);
+        Color randomColor = new Color(R, G, B);
+        graphics.setColor(randomColor);
+    }
+
+    // Canvas
     static int WIDTH = 400;
     static int HEIGHT = 400;
-
 
     public static void main(String[] args) throws InterruptedException {
         JFrame jFrame = new JFrame("Triangles");
@@ -75,8 +87,6 @@ public class Triangles {
         jFrame.setLocationRelativeTo(null);
         jFrame.setVisible(true);
         imagePanel.setBackground(Color.black);
-
-
 
         while(true) {
         imagePanel.repaint();
