1ed4877450d7b1d84c4d8bf5672c7ad9ac517593
baloghdominik <balogh0dominik@gmail.com>
2018-02-27 17:23:26 +0100

    feat(logs): added comments, removed useless  variables

--- Week-03/Day-02/src/logs.java
+++ Week-03/Day-02/src/logs.java
@@ -7,9 +7,9 @@ import java.util.ArrayList;
 public class logs {
     public static void main(String[] args) {
         String pathString = "src/log.txt";
-        ArrayList<String> uniqueip = new ArrayList<String>();
-        unique_ip(pathString, uniqueip);
-        writeUniqueIP(uniqueip);
+        ArrayList<String> uniqueip = new ArrayList<>();
+        unique_ip(pathString, uniqueip); //Collects unique IPs to an arraylist.
+        writeUniqueIP(uniqueip); //Prints out the collected IPs.
     }
 
     private static void writeUniqueIP(ArrayList<String> uniqueip) {
@@ -19,19 +19,19 @@ public class logs {
     }
 
     private static void unique_ip(String pathString, ArrayList<String> uniqueip) {
-        ArrayList<String> log = new ArrayList<String>();
-        ArrayList<String> ip = new ArrayList<String>();
+        ArrayList<String> log = new ArrayList<>();
+        ArrayList<String> ip = new ArrayList<>();
         try {
-            readLog(log, pathString);
-            getIP(log, ip);
-            collectUniqueIP(ip, uniqueip);
+            readLog(log, pathString); //Read the logs.txt to an arraylist.
+            getIP(log, ip); //Collects only IPs from the log.
+            collectUniqueIP(ip, uniqueip); //Removes duplicated IPs
         } catch (IOException e1) {
             e1.printStackTrace();
         }
     }
 
     private static void collectUniqueIP(ArrayList<String> ip, ArrayList<String> uniqueip) {
-        String temp = "";
+        String temp;
         for (int c = 0; c < ip.size(); c++) {
             temp = ip.get(c);
             if (uniqueip.contains(temp)) {
@@ -42,7 +42,7 @@ public class logs {
     }
 
     private static void getIP(ArrayList<String> log, ArrayList<String> ip) {
-        String line2 = "";
+        String line2;
         for (int counter = 0; counter < log.size(); counter++) {
             line2 = log.get(counter);
             line2.split("\\s+");
