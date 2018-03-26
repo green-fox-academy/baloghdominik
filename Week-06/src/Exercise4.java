ae2957490fe9d06fae2240efdd8b409c01459f6a
baloghdominik <balogh0dominik@gmail.com>
2018-03-26 12:09:21 +0200

    feat(Exercise4):Finished

---
+++ Week-06/src/Exercise4.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Exercise4 {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(3, 9, 2, 8, 6, 5));
+
+        numbers.stream()
+                .sorted()
+                .filter(x -> x*x > 20)
+                .forEach(System.out::println);
+    }
+}
