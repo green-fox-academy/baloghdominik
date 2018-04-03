07d17ffc891aecae1dfde77a5cf641d6afe274fb
baloghdominik <balogh0dominik@gmail.com>
2018-04-03 14:30:56 +0200

    feat(springstart):Added Web Greet counter

--- Week-07/Day-01/src/main/java/com/greenfoxacademy/springstart/controller/HelloWebController.java
+++ Week-07/Day-01/src/main/java/com/greenfoxacademy/springstart/controller/HelloWebController.java
@@ -3,14 +3,18 @@ package com.greenfoxacademy.springstart.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class HelloWebController {
 
+    AtomicLong id = new AtomicLong(1);
+
     @RequestMapping("/web/greeting")
     public String greeting(Model model) {
         model.addAttribute("name", " World");
+        model.addAttribute("count", id.getAndIncrement());
         return "greeting";
     }
 }
