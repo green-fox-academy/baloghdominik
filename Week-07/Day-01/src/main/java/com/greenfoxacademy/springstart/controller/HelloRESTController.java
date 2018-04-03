da275faf6cc26d1c8c09817762bec817859134f1
baloghdominik <balogh0dominik@gmail.com>
2018-04-03 14:04:39 +0200

    feat(springstart):Added atomiclong with autoincrement

--- Week-07/Day-01/src/main/java/com/greenfoxacademy/springstart/controller/HelloRESTController.java
+++ Week-07/Day-01/src/main/java/com/greenfoxacademy/springstart/controller/HelloRESTController.java
@@ -4,12 +4,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class HelloRESTController {
 
+    AtomicLong id = new AtomicLong(1);
+
     @RequestMapping(value="/greeting")
     public Greeting greeting(@RequestParam("user") String name){
-        Greeting greeting = new Greeting(1, "Hello, "+name);
+        Greeting greeting = new Greeting(id.getAndIncrement(), "Hello, "+name);
         return greeting;
     }
 }
