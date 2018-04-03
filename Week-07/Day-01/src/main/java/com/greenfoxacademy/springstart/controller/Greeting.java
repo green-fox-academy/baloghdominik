ca5c48be6d57891ee3eb327e2df280cbab02b6e2
baloghdominik <balogh0dominik@gmail.com>
2018-04-03 12:55:07 +0200

    feat(springstart):Added greeting, .../greting?user=yourname

--- Week-07/Day-01/src/main/java/com/greenfoxacademy/springstart/controller/Greeting.java
+++ Week-07/Day-01/src/main/java/com/greenfoxacademy/springstart/controller/Greeting.java
@@ -1,4 +1,21 @@
 package com.greenfoxacademy.springstart.controller;
 
+import java.security.PublicKey;
+
 public class Greeting {
+    private long id;
+    private String content;
+
+    public Greeting(long id, String content){
+        this.id = id;
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public long getId() {
+        return id;
+    }
 }
--- Week-07/Day-01/src/main/java/com/greenfoxacademy/springstart/controller/HelloRESTController.java
+++ Week-07/Day-01/src/main/java/com/greenfoxacademy/springstart/controller/HelloRESTController.java
@@ -1,4 +1,15 @@
 package com.greenfoxacademy.springstart.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloRESTController {
+
+    @RequestMapping(value="/greeting")
+    public Greeting greeting(@RequestParam("user") String name){
+        Greeting greeting = new Greeting(1, "Hello, "+name);
+        return greeting;
+    }
 }
